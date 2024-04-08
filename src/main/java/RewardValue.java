14e64c5ebcb1764779680279a6538fd8e45dad7f
Rahul kumar <152591373+rahulkrchaudhary@users.noreply.github.com>
2024-04-09 00:02:35 +0530

    Update RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,21 +3,13 @@ public class RewardValue {
     double milesValue;
     public RewardValue(double cashValue){
         this.cashValue=cashValue;
-//        this.milesValue=cashToMiles(cashValue);
 
     }
     public RewardValue(float milesValue){
         this.milesValue=milesValue;
-//        this.cashValue=milesTocash(milesValue);
+ 
     }
-//    public double cashToMiles(double cash){
-//        return (cash/0.0035);
-//    }
-//    public double milesTocash(double miles){
-//        return (miles*0.0035);
-//    }
-
-
+  
     public double getCashValue() {
         return cashValue;
     }
